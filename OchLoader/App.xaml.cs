d8f82a0333b40178ae57acf426d9fe53bd7af5c6
me <me@komsa.extern.de>
2016-03-15 15:37:54 +0100

    1. Initial commit ...

---
+++ OchLoader/App.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+using System.Windows;
+using Autofac;
+using OchLoader.View.Main;
+
+namespace OchLoader
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+    private ILifetimeScope _scope;
+
+    private static IContainer container;
+
+    public static IContainer Container
+    {
+      get
+      {
+        if (container == null)
+          Bootstrap();
+
+        return container;
+      }
+
+      private set { container = value; }
+    }
+
+
+    private static void Bootstrap()
+    {
+      ContainerBuilder builder = new ContainerBuilder();
+
+      // Views
+      builder.RegisterType<ApplicationWindow>().SingleInstance();
+
+      // ViewModels
+      builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+        .Where((t) => t.IsSubclassOf(typeof(GalaSoft.MvvmLight.ViewModelBase)))
+        .SingleInstance();
+
+      // Entities
+
+      // Others
+
+      container = builder.Build();
+    }
+
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+      _scope = Container.BeginLifetimeScope();
+      _scope.Resolve<ApplicationWindow>().Show();
+
+      //Messenger.Default.Send();
+    }
+  }
+}
